08971bfb387bd99c929d858ea4f3db1a339cfbb1
vishu39 <vipulsharmak@gmail.com>
2026-01-29 17:08:21 +0530

    .

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ require('dotenv').config({ path: __dirname + `/env/.env.${process.env.NODE_ENV |
 
 require("./startup/db")();
 
+const { setupRoutes } = require('./startup/routes')
+setupRoutes(app)
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-const { setupRoutes } = require('./startup/routes')
-setupRoutes(app)
 
 const PORT = process.env.PORT;
 app.listen(process.env.PORT, () => {
